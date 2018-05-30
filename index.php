41bb238dde4a1882e45ccba6dc47998f22476483
wagaciku <32802472+wagaciku@users.noreply.github.com>
2018-05-30 16:34:02 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>TracRogen Login - Home</title>
+		<link rel="stylesheet" type="text/css" href="/Tracrogen/styles/styles.css">
+	</head>
+	<body >
+		<div class="logo-container">
+			<div class ="logo">
+				<img src="Tracrogen/images/logo/logot.png">
+			</div>
+		</div>
+	</body>
+</html>
